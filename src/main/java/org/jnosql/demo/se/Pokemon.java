5b7910c8e8f6366c5c62fdb4892c70e83e70390e
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-22 16:10:14 +0000

    refact: update dependency to use datafaker
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/jnosql/demo/se/Pokemon.java
+++ src/main/java/org/jnosql/demo/se/Pokemon.java
@@ -1,9 +1,9 @@
 package org.jnosql.demo.se;
 
-import com.github.javafaker.Faker;
 import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
+import net.datafaker.Faker;
 
 import java.util.UUID;
 
